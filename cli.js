7f9fb77151508658d9c9fd95405ff97fa8939f53
Vu Tran <vu@vu-tran.com>
2016-09-05 16:44:59 -0700

    added link/unlink commands

--- cli.js
+++ cli.js
@@ -1,27 +1,46 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const args = require('args');
 const chalk = require('chalk');
-const { api } = require('dext-core-utils');
+const { api, utils } = require('dext-core-utils');
 
-args.command(['install', 'i'], 'Install a new plugin or theme', (name, sub) => {
+args.command(['install', 'i'], 'Install a new plugin or theme.', (name, sub) => {
   const plugin = sub[0];
-  return api.install(plugin)
+  return api.install(plugin, utils.paths.getPluginPath(plugin))
     .then(() => console.log(chalk.green(`${plugin} installed successfully!`)))
     .catch(err => console.error(chalk.red(err)));
 });
 
-args.command(['uninstall', 'u'], 'Uninstall a plugin or theme', (name, sub) => {
+args.command(['uninstall', 'u'], 'Uninstall a plugin or theme.', (name, sub) => {
   const plugin = sub[0];
-  return api.uninstall(plugin)
+  return api.uninstall(plugin, utils.paths.getPluginPath(plugin))
     .then(() => console.log(chalk.green(`${plugin} uninstalled successfully!`)))
     .catch(err => console.error(chalk.red(err)));
 });
 
-args.command(['theme', 't'], 'Sets a theme', (name, sub) => {
+args.command(['theme', 't'], 'Sets a theme.', (name, sub) => {
   const theme = sub[0];
   return api.setTheme(theme)
-    .then(() => console.log(chalk.green(`${theme} has been set successfully!`)))
+  .then(() => console.log(chalk.green(`${theme} has been set successfully!`)))
+  .catch(err => console.error(chalk.red(err)));
+});
+
+args.command(['link'], 'Creates a symlink for the current plugin.', () => {
+  const plugin = path.basename(process.cwd());
+  return api.createSymLink(plugin, process.cwd())
+    .then(data => {
+      console.log(chalk.green(`Linked: ${data.srcPath} -> ${data.destPath}`));
+    })
+    .catch(err => console.error(chalk.red(err)));
+});
+
+args.command(['unlink'], 'Removes the symlink for the current plugin.', () => {
+  const plugin = path.basename(process.cwd());
+  return api.removeSymLink(plugin)
+    .then(data => {
+      console.log(chalk.green(`Unlinked: ${data.destPath}`));
+    })
     .catch(err => console.error(chalk.red(err)));
 });
 
