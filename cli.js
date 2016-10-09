522c7fe73ecd963bbdd1f7c620eb097f51a8d5d8
Enzuo <potc.zone+git@gmail.com>
2016-10-09 22:23:14 +0200

    Add ora spinner animation on install and uninstall (#33)
    Fixes #33 
    
    * Add ora spinner animation on install and uninstall
    * Change spinner text after install and uninstall completion
    * Add spinner for link unlink and theme commands

--- cli.js
+++ cli.js
@@ -3,45 +3,77 @@
 const path = require('path');
 const args = require('args');
 const chalk = require('chalk');
+const ora = require('ora');
 const { api, utils } = require('dext-core-utils');
 
 args.command(['install', 'i'], 'Install a new plugin or theme.', (name, sub) => {
   const plugin = sub[0];
+  const spinner = ora(`${plugin} : Installing...`).start();
   return api.install(plugin, utils.paths.getPluginPath(plugin))
-    .then(() => console.log(chalk.green(`${plugin} installed successfully!`)))
-    .catch(err => console.error(chalk.red(err)));
+    .then(() => {
+      spinner.text = chalk.green(`${plugin} : Installed successfully!`);
+      spinner.succeed();
+    })
+    .catch(err => {
+      spinner.fail();
+      console.error(chalk.red(err));
+    });
 });
 
 args.command(['uninstall', 'u'], 'Uninstall a plugin or theme.', (name, sub) => {
   const plugin = sub[0];
+  const spinner = ora(`${plugin} : Uninstalling...`).start();
   return api.uninstall(plugin, utils.paths.getPluginPath(plugin))
-    .then(() => console.log(chalk.green(`${plugin} uninstalled successfully!`)))
-    .catch(err => console.error(chalk.red(err)));
+    .then(() => {
+      spinner.text = chalk.green(`${plugin} : Uninstalled successfully!`);
+      spinner.succeed();
+    })
+    .catch(err => {
+      spinner.fail();
+      console.error(chalk.red(err));
+    });
 });
 
 args.command(['theme', 't'], 'Sets a theme.', (name, sub) => {
   const theme = sub[0];
+  const spinner = ora(`${theme} : setting theme...`).start();
   return api.setTheme(theme)
-  .then(() => console.log(chalk.green(`${theme} has been set successfully!`)))
-  .catch(err => console.error(chalk.red(err)));
+    .then(() => {
+      spinner.text = chalk.green(`${theme} : Theme has been set successfully!`);
+      spinner.succeed();
+    })
+    .catch(err => {
+      spinner.fail();
+      console.error(chalk.red(err));
+    });
 });
 
 args.command(['link'], 'Creates a symlink for the current plugin.', () => {
   const plugin = path.basename(process.cwd());
+  const spinner = ora(`Linking...`).start();
   return api.createSymLink(plugin, process.cwd())
     .then((data) => {
-      console.log(chalk.green(`Linked: ${data.srcPath} -> ${data.destPath}`));
+      spinner.text = chalk.green(`Linked: ${data.srcPath} -> ${data.destPath}`);
+      spinner.succeed();
     })
-    .catch(err => console.error(chalk.red(err)));
+    .catch(err => {
+      spinner.fail();
+      console.error(chalk.red(err));
+    });
 });
 
 args.command(['unlink'], 'Removes the symlink for the current plugin.', () => {
   const plugin = path.basename(process.cwd());
+  const spinner = ora(`Unlinking...`).start();
   return api.removeSymLink(plugin)
     .then((data) => {
-      console.log(chalk.green(`Unlinked: ${data.destPath}`));
+      spinner.text = chalk.green(`Unlinked: ${data.destPath}`);
+      spinner.succeed();
     })
-    .catch(err => console.error(chalk.red(err)));
+    .catch(err => {
+      spinner.fail();
+      console.error(chalk.red(err));
+    });
 });
 
 args.command(['config'], 'Display the raw config.', () => api.getConfig()
